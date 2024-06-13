820d4724836ac72c07f4f36447dbb82c451b4e15
Victor Pinho <vtsppinho@gmail.com>
2024-06-13 18:21:21 -0300

    [feat] Implementa pattern strategy para definir o nome do campo em objeto do tipo class

--- src/main/java/br/github/vtspp/converter/ClassConverter.java
+++ src/main/java/br/github/vtspp/converter/ClassConverter.java
@@ -2,29 +2,12 @@ package br.github.vtspp.converter;
 
 import br.github.vtspp.converter.util.ReflectionConverterUtil;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.lang.reflect.Field;
 
 public class ClassConverter<T> extends ConvertableToMap<T> {
 
     @Override
-    public Map<String, Object> toMap(T obj, boolean skipNullValues) {
-        Objects.requireNonNull(obj, "O objeto a ser convertido não pode ser nulo");
-
-        final var objToMap = new LinkedHashMap<String, Object>();
-        final var fields = obj.getClass().getDeclaredFields();
-
-        Arrays.stream(fields).forEach(field -> {
-            final var value = extractValue(obj, ReflectionConverterUtil.toFieldClassName(field));
-
-            if (Objects.isNull(value) && skipNullValues)
-                return;
-
-            objToMap.put(field.getName(), value);
-        });
-
-        return objToMap;
+    protected String fieldNameByStrategy(Field field) {
+        return ReflectionConverterUtil.toFieldClassName(field);
     }
 }
